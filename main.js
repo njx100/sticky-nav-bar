fe6cb34b1b4b1e8a0fa55665254ff40e4f9fc479
njx100 <dnnguyen100@gmail.com>
2023-03-29 10:24:33 +0700

    complete project

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+const navBarEl = document.querySelector(".navbar");
 
+const bottomContainerEl = document.querySelector(".bottom-container");
+
+// console.log(bottomContainerEl.offsetTop);
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > bottomContainerEl.offsetTop - navBarEl.offsetHeight - 50) {
+        navBarEl.classList.add("active");
+    } else {
+        navBarEl.classList.remove("active");
+    }
+})
